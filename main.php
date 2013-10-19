96091ec7354588127c29cda0916d5d5a423009cb
Somin Kobayashi <somin@oheso.com>
2013-10-19 12:51:31 +0900

    とりあえずの1枚ページ

---
+++ main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>ニモニク - 株式会社ニーモニック</title>
+	<style>
+		body {
+			background: #f08000;
+			margin: 0;
+			padding: 0;
+		}
+		h1 {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			text-align: center;
+			margin-top: -100px;
+			margin-left: -73px;
+		}
+	</style>
+</head>
+<body>
+	<h1><img src="logo.png" alt="ニモニク"></h1>
+</body>
+</html>
